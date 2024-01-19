73566757af6fde4a51a90e1b0faf394e050b2388
Krisztian Nemeth <krsnmth@gmail.com>
2024-01-19 15:29:48 +0000

    working on the CV download bug

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -170,7 +170,7 @@ function Header({ navBarOpen, setNavBarOpen, handleDarkMode }) {
                 </Link>
               </li>
               <li className='z-50 mx-5 lg:inline-block'>
-                <a href='public/CV/Krisztian-Nemeth-CV.pdf' download>
+                <a href='/CV/Krisztian-Nemeth-CV.pdf' download>
                   <Button>Download CV</Button>
                 </a>
               </li>
